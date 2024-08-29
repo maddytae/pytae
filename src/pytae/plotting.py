cdd6525e74a6a6c7c9ab190c93ee61dc8ba24f53
Madhukar Kumar Jha <madhukar.k.jha@gmail.com>
2024-08-29 12:09:45 -0400

    saving work

--- src/pytae/plotting.py
+++ src/pytae/plotting.py
@@ -254,78 +254,8 @@ class Plotter:
 
         return pivot_table
 
-    def finalize(self, consolidate_legends=False, bbox_to_anchor=(0.8, -0.05), ncols=10):
-        self.consolidate_legends = consolidate_legends
-        self.bbox_to_anchor = bbox_to_anchor
-        self.ncols = ncols
     
 
-        for ax in self.fig.axes:
-
-            # Initially hide all spines
-            ax.spines['top'].set_position(('outward', 5))
-            ax.spines['bottom'].set_position(('outward', 5))
-            ax.spines['left'].set_position(('outward', 5))
-            ax.spines['right'].set_position(('outward', 5))
-            
-            for spine in ax.spines.values():
-                spine.set_visible(False)
-
-            # Check and conditionally show spines based on label presence
-            for spine, axis, label_position in [
-                ('top', ax.xaxis, 'top'),
-                ('bottom', ax.xaxis, 'bottom'),
-                ('left', ax.yaxis, 'left'),
-                ('right', ax.yaxis, 'right')
-            ]:
-                # Get labels for this axis
-                labels = ax.get_xticklabels() if axis == ax.xaxis else ax.get_yticklabels()
-                labels = [label.get_text() for label in labels if label.get_text()]
-                
-                # Check if labels are present and not just default values
-                if (label_position == axis.get_label_position() and 
-                    labels and 
-                    (labels != ['0.0', '0.2', '0.4', '0.6', '0.8', '1.0'] or spine == 'right')):
-                    ax.spines[spine].set_visible(True)     
-            
-            secondary_ax = getattr(ax, 'right_ax', None)
-
-        
-            # Manage the legend for the current axis
-            if not self.consolidate_legends:
-                if ax.get_label(): #that means it is primary ax; label is absent for secondary ax
-                    # ax.legend(frameon=False)
-                    handles, labels = ax.get_legend_handles_labels()
-                    labels = [label.replace(" (right)", "") for label in labels]
-                    if secondary_ax==None:
-                        ax.legend(handles, labels, frameon=False, loc='best')
-                    else:
-                        ax.legend(handles, labels, frameon=False, loc='upper left') # in case if it is secondary ax then always put on uppler left else put in best location
-                if not ax.get_label():
-                    handles, labels = ax.get_legend_handles_labels()
-                    labels = [label.replace(" (right)", "") for label in labels]
-                    ax.legend(handles, labels, frameon=False, loc='upper right')
-                    
-
-            else:
-                try:
-                    ax.get_legend().remove()
-                except:
-                    None
-                
-        handles, labels = self._collect_handles_and_legends()
-                    
-    
-        if self.consolidate_legends:
-            # Add a single consolidated legend to the figure
-            self.fig.legend(handles, labels, bbox_to_anchor=self.bbox_to_anchor, ncol=self.ncols, frameon=False)
-    
-        # Adjust the layout
-        self.fig.tight_layout()
-    
-        return self        
-        
-    
 
     def _handle_data_output(self, data):
         if self.print_data:
@@ -358,5 +288,94 @@ class Plotter:
         """Stores the plot_dict kwargs in a dictionary with the axis as the key."""
         self.plot_kwargs_store[ax.get_label()] = plot_dict
 
+    def finalize(self, consolidate_legends=False, bbox_to_anchor=(0.8, -0.05), ncols=10):
+        self.consolidate_legends = consolidate_legends
+        self.bbox_to_anchor = bbox_to_anchor
+        self.ncols = ncols
 
-     
+        for ax in self.fig.axes:
+            ax_label=ax.get_label()
+            if '<colorbar>' in ax_label or ax_label == '':
+                continue
+
+            # Initially hide all spines
+            ax.spines['top'].set_position(('outward', 5))
+            ax.spines['bottom'].set_position(('outward', 5))
+            ax.spines['left'].set_position(('outward', 5))
+            ax.spines['right'].set_position(('outward', 5))
+            
+            for spine in ax.spines.values():
+                spine.set_visible(False)
+
+            # ax.tick_params(axis='y', labelleft=False,labelright=False)
+            # ax.tick_params(axis='x', labeltop=False,labelbottom=False)
+
+        for ax in self.fig.axes:
+            # print(ax.get_label())
+            if '<colorbar>' in ax_label or ax_label == '':
+                continue     
+            
+
+
+
+            
+
+            # Check and conditionally show spines based on label presence
+            default_labels = ['0.0', '0.2', '0.4', '0.6', '0.8', '1.0']
+            for spine, axis, label_position in [
+                ('top', ax.xaxis, 'top'),
+                ('bottom', ax.xaxis, 'bottom'),
+                ('left', ax.yaxis, 'left'),
+                ('right', ax.yaxis, 'right')
+            ]:
+                # Get labels for this axis
+                labels = ax.get_xticklabels() if axis == ax.xaxis else ax.get_yticklabels()
+                labels = [label.get_text() for label in labels if label.get_text()]
+                
+                # Check if labels are present and not just default values
+                if (label_position == axis.get_label_position() and 
+                    labels and 
+                    (labels != default_labels) ):
+                    ax.spines[spine].set_visible(True) 
+                    
+                # Hide tick labels and ticks if they match the default labels
+                if labels == default_labels:
+                    tick_params_position = 'labeltop' if label_position == 'top' else (
+                        'labelbottom' if label_position == 'bottom' else (
+                        'labelleft' if label_position == 'left' else 'labelright'
+                    ))
+                    ax.tick_params(
+                        axis='x' if axis == ax.xaxis else 'y', 
+                        which='both',        # Apply to both major and minor ticks
+                        length=0,            # Hide the ticks by setting length to 0
+                        **{tick_params_position: False}  # Hide the tick labels
+                    )
+            
+
+            is_secondary = '^' in ax.get_label()           
+            count = sum(1 for k in self.plot_kwargs_store if k.replace('^', '') == ax.get_label().replace('^',''))
+
+            
+            handles, labels = ax.get_legend_handles_labels()
+            labels = [label.replace(" (right)", "") for label in labels]
+            if is_secondary and count>1:
+                ax.legend(handles, labels, frameon=False, loc='upper right')
+            elif count>1:
+                ax.legend(handles, labels, frameon=False, loc='upper left')
+            else:
+                ax.legend(handles, labels, frameon=False, loc='best')  
+            if self.consolidate_legends:
+                ax.get_legend().remove()
+    
+
+                
+        handles, labels = self._collect_handles_and_legends()
+                    
+    
+        if self.consolidate_legends:
+            # Add a single consolidated legend to the figure
+            self.fig.legend(handles, labels, bbox_to_anchor=self.bbox_to_anchor, ncol=self.ncols, frameon=False)    
+
+        self.fig.tight_layout()
+        return self
+
